d2b73d6271cdad1c0e50c7540fb4a7db08cd6bcf
root <gargkanish23@gmail.com>
2017-07-22 18:46:44 +0530

    gallery error

--- components/spic.js
+++ components/spic.js
@@ -18,6 +18,7 @@ $('ul.nav li.dropdown').hover(function() {
 }, function() {
   $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
 });
+
 $(window).on('hashchange', function() {community();});
 function community() {
     str=window.location.hash;

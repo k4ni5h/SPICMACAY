e3ca908c6dd4d0b5f4fdf6d06548c3ea7b71a501
k4ani5h <gargkanish23@gmail.com>
2017-12-31 17:52:38 +0530

    pratyush

--- components/home.js
+++ components/home.js
@@ -1,7 +1,7 @@
 var student={
   4:[['Priyansh Kaushal','priyansh'],['Shambhavi Pandey','shambhavi'],['Apoorva Singhal','apoorva'],['Chandana Shree Mounika','chandana'],['Nimisha Gupta','nimisha'],['Alekhya Payla','alekhya'],['Nainsu Riya','nainsu'],['Ujjwal Gahlot','ujjwal'],['Ritesh Chauhan','ritesh'],['Chirag Gupta','chirag'],['Rahul Agarwal','rahul'],['Ajay Kumar','ajay'],['Kanhaiya Jaju','jaju'],['Arindham Purohit','arindham'],['Reshi Kiran NC','reshi'],['Pankaj Bansal','pankaj'],['Karan Verma','karan'],['Parag Jain','parag']],
   3:[['Halember Manasa','manasa'],['Sejal Wasnik','sejal'],['Shruti Jamgade','shruti'],['Samila Subba','samila'],['Divya Kumawat','divya'],['Shivani Bhakal','shivani'],['Sanjana Garg','sanjana'],['Akshit Goel','akshit'],['Shahjad Ali','shahjad'],['Aryan Yadav','aryan'],['Adarsh Goyal','adarsh'],['Aman Azad','aman'],['Dikshit Gupta','dikshit'],['Anshul Dhunna','anshul'],['Deepak Sen','deepak'],['Deept Shukla','deept'],['Vaibhav Gupta', 'vaibhav3']],
-  2:[['Kanish','kanish'],['Tanpreet Singh Kalsi','tanpreet'],['Sakshi Priya','sakshi'],['Kapil Birthala','jaipur'],['Vishal Purohit','vishal'],['Bharat Joshi','bharat'],['Pratyush Jha','jha'],['Sneha Maheshwari','mahishmati'],['Shazeb Ata','shazeb'],['Mohit Aneja','mohit'],['Sourab Yadav','sourab'],['Naman Gupta','naman'],['Anirudh Garg','anirudh'],['Vaibhav Nayer','vaibhav2']],
+  2:[['Kanish','kanish'],['Tanpreet Singh Kalsi','tanpreet'],['Sakshi Priya','sakshi'],['Kapil Birthala','jaipur'],['Vishal Purohit','vishal'],['Bharat Joshi','bharat'],['Sneha Maheshwari','mahishmati'],['Shazeb Ata','shazeb'],['Mohit Aneja','mohit'],['Sourab Yadav','sourab'],['Naman Gupta','naman'],['Anirudh Garg','anirudh'],['Vaibhav Nayer','vaibhav2']],
   1:[]
 };
 
@@ -68,7 +68,12 @@ function abcd1(){
   }
   for(i=0;i<student[2].length;i++)
   {
-    $( "#student_2" ).append( '<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6 animate-bottom"><div><img class="img-circle center-block photo" src="images/team/volunteers/' + student[2][i][1] + '.jpg"></div><div class="text-center h4">' + student[2][i][0] + '</div></div>' );
+    if(i==2) {
+      $("#student_2").append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6 animate-bottom"><div class="img-circle" style=""><img style="width: 40%;height: 100px;float: left;border-right: 0;margin-left: 10%;" class="center-block photo" src="images/team/volunteers/sakshi.jpg"><img class="center-block photo" style="width: 40%;height: 100px;float: left;border-left: 0;" src="images/team/volunteers/jha.jpg"></div><div class="text-center h4">Sakshi &amp; Pratyush</div></div>');
+    }
+    else {
+      $( "#student_2" ).append( '<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6 animate-bottom"><div><img class="img-circle center-block photo" src="images/team/volunteers/' + student[2][i][1] + '.jpg"></div><div class="text-center h4">' + student[2][i][0] + '</div></div>' );
+    }
   }
   if(student[1].length==0) {
     $( "#student_1" ).html("<center>coming soon</center>");
